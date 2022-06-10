7fb67b7f6f0d7e5292fd6484131b5f09074d61c6
nictheriscer <nicolas.riscalas@stu.ocsb.ca>
2022-06-10 09:47:22 -0400

    fdsafsda

--- code.py
+++ code.py
@@ -1,11 +1,12 @@
 
 #!/usr/bin/env python3
-# Created By: Alex De Meo
+# Created By: Nicolas Riscalas
 # Date: 03/25/2022
-# Description: This is my CPT game for the edgebadge
+# Description: CPT game EdgeBadge
 
 import ugame
 import stage
+import constants
 
 
 def game_scene():
@@ -14,21 +15,53 @@ def game_scene():
     image_bank_background = stage.Bank.from_bmp16(
         "space_aliens_background.bmp"
     )
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
     # creates the 10 by 8 image grid, sets the background image to background
-    background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
 
+    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
     # 60 means it will update it 60 times a second
-    game = stage.Stage(ugame.display, 60)
+    game = stage.Stage(ugame.display, constants.FPS)
     # accesses the first layer(background) and makes the list of images for the background
-    game.layers = [background]
+    game.layers = [ship] + [background]
     # takes layers and shows them on the screen
     game.render_block()
     # this is the game loop so it is supposed to loop forever
     while True:
         # Get user input
-
+        keys = ugame.buttons.get_pressed()
+        # When button b pressed print that it is pressed
+        if keys & ugame.K_X:
+            print("button B pressed")
+        # When button a is pressed print that it is pressed
+        if keys & ugame.K_O:
+            print("button A pressed")
+        # When the start button is pressed print that it is pressed
+        if keys & ugame.K_START:
+            print("Start")
+        # when the right button is pressed move right
+        if keys & ugame.K_RIGHT:
+            if ship.x != constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 1, ship.y)
+                print("right")
+        # when the left button is pressed move left 
+        if keys & ugame.K_LEFT:
+            if ship.x != 0:
+                ship.move(ship.x - 1, ship.y)
+                print("left")
+        # when the up button is pressed move up
+        if keys & ugame.K_UP:
+            if ship.y != 0:
+                ship.move(ship.x, ship.y - 1)
+                print("up")
+        # when the down button is pressed move down
+        if keys & ugame.K_DOWN:
+            if ship.y != constants.SCREEN_Y - constants.SPRITE_SIZE:
+                ship.move(ship.x, ship.y + 1)
+                print("down")
+        game.render_sprites([ship])
+        game.tick()
         # update game logic
-        pass
 
 
 if __name__ == "__main__":
---
+++ constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Nicolas Riscalas
+# Created on: June 2022
+# This is the constants file for CPT
+
+# PyBadge screen size is 160 x 128
+# set the x max
+SCREEN_X = 160
+# set the y max
+SCREEN_Y = 128
+# set the grid x max
+SCREEN_GRID_X = 10
+# set the grid y max
+SCREEN_GRID_Y = 8
+# set the sprite max size
+SPRITE_SIZE = 16
+# set the maximum aliens
+TOTAL_NUMBER_OF_ALIENS = 5
+# set the max FPS
+FPS = 60
+# set the sprite's movement speed
+SPRITE_MOVEMENT_SPEED = 1
